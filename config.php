6c111205e9d98b8fc605c83deba407d19e91dbeb
Shivam_Singh <shivamsingh6490@gmail.com>
2025-04-03 22:37:14 +0530

    added database credentials

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+class DbCredentials{
+    protected function hostName(){
+        return "localhost";
+    }
+    protected function userName(){
+        // return "u899926382_reliefa1";
+        return "root";
+    }
+    protected function password(){
+        // return "Reliefa@123";
+        return "";
+    }
+    protected function dbName(){
+        // return "u899926382_reliefa1";
+        return "recipe";
+    }
+}
+?>
